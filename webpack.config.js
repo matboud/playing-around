680e19fc8ee267d3cf3150fd4e07af6079efd321
Matboud <m.matboud@gmail.com>
2021-05-30 20:24:37 +0100

    init webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
+
+}
